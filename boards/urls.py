bb922c5b2daa7f3780805b7b7346614009f14ce9
Filipe Vasconcelos <filipev@me.com>
2022-05-16 15:13:22 -0300

    view tests + bootstrap

--- boards/urls.py
+++ boards/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-
 from boards import views
 
 urlpatterns = [
-    path('',views.home, name="home")
+    path('', views.home, name="home")
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
-from django.http import HttpResponse
 
 from .models import Board
 
