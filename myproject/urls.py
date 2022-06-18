a60b70c57d3ba8ceac32da07055a1c972a712ef7
Filipe Vasconcelos <filipev@me.com>
2022-06-18 20:35:55 -0300

    part 4 complete

--- myproject/urls.py
+++ myproject/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('boards.urls')),
-    path('signup/', include('accounts.urls')),
+    path('', include('accounts.urls')),
 ]
