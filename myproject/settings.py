3b5b21492c71ca1f115b7a3be1bcd6ef53241417
Filipe Vasconcelos <filipev@me.com>
2022-06-18 16:47:46 -0300

    partial part 4

--- myproject/settings.py
+++ myproject/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'widget_tweaks',
     'boards',
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- myproject/urls.py
+++ myproject/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('boards.urls'))
+    path('', include('boards.urls')),
+    path('signup/', include('accounts.urls')),
 ]
