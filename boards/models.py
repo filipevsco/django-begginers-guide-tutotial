b469ddb64e5da608e83c6b6a6865939fadc68866
Filipe Vasconcelos <filipev@me.com>
2022-06-21 13:17:52 -0300

    add markdown

--- boards/models.py
+++ boards/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.text import Truncator
 from django.contrib.auth.models import User
+from django.utils.html import mark_safe
+from markdown import markdown
 
 
 class Board(models.Model):
@@ -34,6 +36,10 @@ class Post(models.Model):
     update_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts') ###
     updated_by = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='+') ####
+
     def __str__(self):
         truncated_message = Truncator(self.message)
         return truncated_message.chars(30)
+
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.message, safe_mode='escape'))
